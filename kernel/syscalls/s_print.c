fbd5950dc90c112542bd498980e8e16b669626fd
Menna <menna@synapse>
2012-10-28 01:36:00 +0200

    system calls doxy documentation

--- kernel/syscalls/s_print.c
+++ kernel/syscalls/s_print.c
@@ -1,9 +1,26 @@
+/**
+ * @addtogroup Interrupts-and-Syscalls
+ * @{
+ * @file s_print.c
+ * @author Menna Essa
+ * @date Thursday 25/10/2012
+ * @brief The actual printf procedure -to be implemented- .
+ * @name  Print procedure .
+ * @{
+ *
+ */
 #include <types.h>
 #include <arch/x86/x86.h>
 #include <syscalls/syscalls.h>
-
+/**
+ *@fn int32_t sys_printf(char * str)
+ *@param str : format string to print
+ *@return exit status. 
+ *@brief The actual print procedure requested by the printf system call.  
+ */
 int32_t sys_printf(char * str)
 {
 	printk("Print called , to be implemented \n");
 	return 0;
 }
+ /** @} @} */
--- kernel/syscalls/syscalls.c
+++ kernel/syscalls/syscalls.c
@@ -1,3 +1,15 @@
+/**
+ * @addtogroup Interrupts-and-Syscalls
+ * @{
+ * @file syscalls.c
+ * @author Menna Essa
+ * @date Thursday 25/10/2012
+ * @brief maps calls into their respective handlers.
+ * @name System calls handler .
+ * @{
+ *
+ */
+
 #include <types.h>
 #include <test.h>
 #include <sys.h>
@@ -5,9 +17,24 @@
 #include <arch/x86/cpu_state.h>
 #include <syscalls/syscalls.h>
 
+/**
+ * @brief Declare system calls table with pointers to calls handlers
+ */
 fnptr_t sys_call_table[] =  { sys_printf };
+/**
+ * @brief keeps the size of the table for boundary checking.
+ */
 int32_t n_calls = sizeof(sys_call_table)/sizeof(int32_t); 
+/**
+ * @brief Keeps error codes to return to the caller.
+ */
 int32_t s_errno=0;
+/**
+ *@fn int32_t map_syscall(cpu_state_t *cpu_state);
+ *@param cpu_state : pointer to cpu_state_t structure
+ *@return exit status / error code.
+ *@brief maps the system call by offseting the sys_call_table structure using the offset in eax register.
+ */
 int32_t map_syscall(cpu_state_t *cpu_state)
 {
 	printk("System call issued \n");
